729d529a9d78d4e7ca71dd8110d149a994b1165c
José Eduardo Esteva Córdova <zbrm34@gmail.com>
2024-06-17 00:17:28 -0600

    Se agregó la configuración para sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,33 @@
 from flask import Flask
 from flask_restful import Api
 from .routes import APIRoutes
-
+from .config import Config
+from .extensions import db
 
 # Creamos una función para montar el servidor
 def crear_app():
   # La variable app contendrá todo nuestro servidor
   app = Flask(__name__)
+  # Le indicamos a nuestra app que se configura a través de un objeto
 
-  # La variable API manejará todas las peticiones hacia nuestro servir
-  api = Api(app)
-  # Agregamos una variable va a manejar las rutas
-  routes = APIRoutes()
-  # Inicializamos las rutas en nuestra API
-  routes.init_routes(api)
+  # Se iba a configurar a través de un objeto
+  app.config.from_object(Config)
 
+  # Conectamos la App con la base de datos
+  db.init_app(app)
+  
+  # Antes de que se cree el acceso a la API, 
+  # se ejecuta primero esto
+  with app.app_context():
+    # Inicializamos la DB
+    db.create_all()
+
+    # La variable API manejará todas las peticiones hacia nuestro servir
+    api = Api(app)
+    # Agregamos una variable va a manejar las rutas
+    routes = APIRoutes()
+    # Inicializamos las rutas en nuestra API
+    routes.init_routes(api)
 
   # Regresamos ese servidor montado
   return app

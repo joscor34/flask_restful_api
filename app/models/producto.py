8513ca23dd37616b7899adde4f36d79ee9bfeb97
José Eduardo Esteva Córdova <zbrm34@gmail.com>
2024-06-17 00:20:28 -0600

    El modelo del producto se creó

---
+++ app/models/producto.py
@@ -0,0 +1,11 @@
+# Importamos la instancia de nuestra DB
+from app.extensions import db
+
+class Producto(db.Model):
+  __tablename__ = 'objetos_almacen'
+  id = db.Column(db.Integer, primary_key=True)
+  nombre = db.Column(db.String(50), nullable=False)
+  cantidad = db.Column(db.Integer, nullable=False)
+
+  def __repr__(self):
+    return f'<Producto {self.nombre}>'

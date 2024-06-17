b39d8e27730ed16ba0d37c88faef73a7b323002b
José Eduardo Esteva Córdova <zbrm34@gmail.com>
2024-06-17 00:18:25 -0600

    Se cambio la configuración de la ruta almacén

--- app/routes.py
+++ app/routes.py
@@ -6,26 +6,6 @@ from flask import request, jsonify
 from .methods import *
 
 
-
-lista_objetos_almacen = [
-{
-  'id':1,
-  'nombre': 'Lapiz',
-  'cantidad': 4
-},
-{
-  'id':2,
-  'nombre': 'Goma',
-  'cantidad': 3
-},
-{
-  'id':3,
-  'nombre': 'Tijera',
-  'cantidad': 6
-}
-]
-
-
 # Creamos una clase que va a heredar atributos del modulo "Resource"
 class HelloWorld(Resource):
   # Este método se va a ejecutar cuando el usuario acceda a cierta ruta a través del método GET  
@@ -43,17 +23,14 @@ class Almacen(Resource):
     parametro_id = request.args.get('id')
     parametro_nombre = request.args.get('nombre')
 
-    return buscar_elemento_id_nombre(lista_objetos_almacen, parametro_id, parametro_nombre)
+    return buscar_elemento_id_nombre(parametro_id, parametro_nombre)
   # Ponemos un nuevo objeto en el almacén
   # Enviarle informacion a la API mediante el cliente
   def post(self):
     # Se crea una nueva variable para guardar la información que Posteo el ususario
     data = request.get_json()
 
-    #Agregamos la informacion a lista del almacen
-    lista_objetos_almacen.append(data)
-
-    return { 'received': True, 'status':200, 'info':data }
+    return crear_producto(data['nombre'], data['cantidad'])
 
 
 # Creamos una clase que va a manejar todas las rutas
